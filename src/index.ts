e385a432e55864d6f4436059e01564fcccbc22fb
atulkumbhar <atulkumbhar.edu@gmail.com>
2021-05-21 11:37:59 +0530

    3rd

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,6 @@ export class Hello {
   public sayHello() {
     return 'hello, world!';
 
-
-
-
-    HTMLAnchorElementkok
-    new kms.Key(this, "id", {
-      enableKeyRotation: false,
     })
   }
 }
